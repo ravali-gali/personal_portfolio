732dee1377fb9ca66c27863e4f580240793e0910
ravali-gali <Eshaweb1711@gmail.com>
2024-11-08 12:17:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
     });
-
+    
     $(window).on('scroll load', function () {
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
